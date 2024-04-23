9ad959596d8dfaac863a7fe085189c4e3b923ad2
Eisha Tir Raazia <eisha.mazhar19@gmail.com>
2024-04-24 01:36:20 +0200

    move files out of taskbox

--- taskbox/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css'
-
-import './assets/main.css'
-
-createApp(App).mount('#app')
